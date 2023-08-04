e9f1286eab074aff37288ec893782bd545cb200a
Jhosel Badillo Cortes <jhosel.badillo.cortes@gmail.com>
2023-08-04 07:14:25 -0600

    Initial Commit

---
+++ ChartsExample/ChartsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsExampleApp.swift
+//  ChartsExample
+//
+//  Created by Jhosel Badillo Cortes on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChartsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartsExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChartsExample
+//
+//  Created by Jhosel Badillo Cortes on 04/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
