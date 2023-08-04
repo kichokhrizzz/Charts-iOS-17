44d84fcc9676caf682727688fbdc6372f4b714c5
Jhosel Badillo Cortes <jhosel.badillo.cortes@gmail.com>
2023-08-04 11:47:40 -0600

    first commit

---
+++ ChartsExample/Charts/BarChart.swift
@@ -0,0 +1,18 @@
+//
+//  BarChart.swift
+//  ChartsExample
+//
+//  Created by Jhosel Badillo Cortes on 04/08/23.
+//
+
+import SwiftUI
+
+struct BarChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarChart()
+}
---
+++ ChartsExample/Charts/PieChart.swift
@@ -0,0 +1,18 @@
+//
+//  PieChart.swift
+//  ChartsExample
+//
+//  Created by Jhosel Badillo Cortes on 04/08/23.
+//
+
+import SwiftUI
+
+struct PieChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PieChart()
+}
